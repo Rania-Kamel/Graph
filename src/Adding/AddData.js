4925916e2c709da9188ab43084f37b0b4f46ad6e
rania kamel <raniakamel706@gmail.com>
2021-08-10 11:51:19 +0200

    first comment

---
+++ src/Adding/AddData.js
@@ -0,0 +1,86 @@
+import{css , cx} from '@emotion/css'
+import React, { useState } from 'react';
+
+function AddData({AddNewData , setAddNewData}) {
+  const style = css`
+  form {
+    width: fit-content;
+    margin: 10px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .input{
+    height: 25px;
+    margin-left: 10px;
+
+  }
+  .button{
+    width: fit-content;
+    margin: 10px;
+    padding: 5px 20px;
+    font-size: 20px;
+  }
+
+  `
+ 
+  const [name , setName] = useState();
+  const [score , setScore] = useState();
+
+  
+const handelsubmit = () => {
+  
+  let number = parseInt(score, 10);
+
+  setAddNewData([...AddNewData , 
+ { 
+    name,
+    score : number 
+
+}
+  ]
+   
+  );
+
+  
+
+  
+  
+};
+
+
+  return (
+    <div className={cx(style)}>
+     
+    < form onSubmit = {(e) => {
+       e.preventDefault();
+        handelsubmit()
+        e.target.elements.name.value=""
+        e.target.elements.score.value=""
+         } }>
+            <input type = "text"
+            autoFocus = "on"
+            autoComplete = "off"
+            placeholder = "Enter name ...."
+            className="input"
+            name = "name"
+            onChange={e => {setName(e.target.value)}}
+            /> 
+            <input type = "number"
+            placeholder = "Enter score ...."
+            className="input"
+            name = "score"
+            onChange={e => {setScore(e.target.value)}}
+            /> 
+            <input type = "submit" className="button"
+            value = "Add" />
+            
+            </form>
+       
+     </div>
+  
+     
+  );
+}
+
+export default AddData;
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import{css , cx} from '@emotion/css'
+import AddData from './Adding/AddData'
+import React, { useState } from 'react';
+
+const data = [
+  {name : "ishak" , score : 25},
+  {name : "rania" , score :190},
+  {name : "mariam" , score : 75},
+  {name : "mena" , score : 100},
+  {name : "thomas" , score : 150},
+  
+
+ ]
 
 function App() {
+
+  const [AddNewData , setAddNewData] = useState(data);
+
+  
+  const style = css`
+  
+    margin: auto;
+    width: fit-content;
+    height: 220px;
+    margin-top: calc(50vh - 210px);
+    border-bottom: 2px solid black;
+    border-left: 2px solid;
+    display: flex;
+    flex-flow: wrap-reverse;
+    .name{
+      transform: rotate(90deg);
+      font-size: 20px;
+      font-style: oblique;
+      font-weight: 500;
+  } 
+   
+
+  `
+ 
+   const styling = (score , index) =>{
+
+    const style = css`
+  
+      width: 40px;
+      margin: 0 10px;
+      position: relative;
+      animation : move${index} 2.5s linear forwards;
+      @keyframes move${index} {
+        from {height: 0px;}
+        to {height: ${score}px;}
+      }
+     
+    `
+    return style;
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <AddData AddNewData = {AddNewData} setAddNewData = {setAddNewData} />
+
+     <div className={cx(style)}>
+    {AddNewData.map(({score , name} , index) => (
+     
+      
+       <div className={cx(styling(score , index))} style={{height: score , backgroundColor: "#" + score + "A"  }}>
+       <div className="score" style={{marginTop : -13 ,  marginLeft: -60*(index+1)}}>{score}</div>
+       <div className="name" style={{marginTop : score + 20  }}>{name}</div>
+       </div>
+       
+       
+     
+      
+      
+      
+    )
+
+    )}
     </div>
+       
+     </>
+  
+     
   );
 }
 
