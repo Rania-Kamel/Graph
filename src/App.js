c0d25b9709bfce442f9125885136d453f8a49550
ishak saad <ishakprogramingismylife@gmail.com>
2021-08-11 11:32:06 +0200

    Ishka El gamed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import{css , cx} from '@emotion/css'
+import { css, cx } from '@emotion/css'
 import AddData from './Adding/AddData'
 import React, { useState } from 'react';
 
 const data = [
-  {name : "ishak" , score : 25},
-  {name : "rania" , score :190},
-  {name : "mariam" , score : 75},
-  {name : "mena" , score : 100},
-  {name : "thomas" , score : 150},
-  
+  { name: "ishak", score: 25 },
+  { name: "rania", score: 10 },
+  { name: "mariam", score: 75 },
+  { name: "mena", score: 100 },
+  { name: "thomas", score: 150 },
 
- ]
+
+]
 
 
 function App() {
 
-  const [AddNewData , setAddNewData] = useState(data);
+  const [AddNewData, setAddNewData] = useState(data);
 
-  
+
   const style = css`
   
     
@@ -31,14 +31,18 @@ function App() {
     
     .columnAndName{
       display: flex;
-      flex-direction: column-reverse;
+      flex-direction: column;
+      align-self: flex-end;
+      margin-bottom: -67px;
     }
     .name{
       transform: rotate(90deg);
       font-size: 20px;
       font-style: oblique;
       font-weight: 500;
-     
+      width: 100px;
+      margin-top: 40px;
+      margin-left: -17px;
       
   } 
   .number{
@@ -48,8 +52,8 @@ function App() {
    
 
   `
- 
-   const styling = (score , index) =>{
+
+  const styling = (score, index) => {
 
     const style = css`
    
@@ -65,32 +69,31 @@ function App() {
      
     `
     return style;
-   }
+  }
 
-   const nummers = () => {
-   const number = []
-    for (var i = 300; i > 0; i-=50) {
-      number.push(<div style={{marginLeft: -35 }}>{i}</div>)
+  const nummers = () => {
+    const number = []
+    for (var i = 300; i > 0; i -= 50) {
+      number.push(<div style={{ marginLeft: -35 }}>{i}</div>)
     }
     return number
-   
+
   };
 
   return (
     <>
-     <AddData AddNewData = {AddNewData} setAddNewData = {setAddNewData} />
-     <div className={cx(style)}>
-    <div className="number"> {nummers()}</div>
-    {AddNewData.map(({score , name} , index) => (
-     <div className="columnAndName">
-       
-       <div className={cx(styling(score , index))} style={{height: score , backgroundColor: "#" + score + "A"  }}></div>
-       <div className="name" style={{}}>{name}</div>
-       </div>
-    )
-    )}
-     </div>
-     </>
+      <AddData AddNewData={AddNewData} setAddNewData={setAddNewData} />
+      <div className={cx(style)}>
+        <div className="number"> {nummers()}</div>
+        {AddNewData.map(({ score, name }, index) => (
+          <div className="columnAndName">
+            <div className={cx(styling(score, index))} style={{ height: score, backgroundColor: "#" + score + "A" }}></div>
+            <div className="name" >{name}</div>
+          </div>
+        )
+        )}
+      </div>
+    </>
   );
 }
 
